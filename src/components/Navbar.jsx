ee48d9a281744d79e8bdbae701add298fe928f9c
hassamarif <arifhassam18@gmail.com>
2023-03-26 01:55:26 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai"
+import { useState } from 'react';
+
+function Navbar() {
+  const [nav, setNav] = useState(true)
+
+  const handleNav=()=>{
+    setNav(!nav)
+  }
+  return (
+    <div className='flex justify-between p-4 my-4 mx-auto max-w-[1240px] text-white'>
+      <h1 className='w-full text-3xl font-bold text-[#59dda8] pt-3'>CALCULATOR!</h1>
+      <ul className='hidden md:flex'>
+        <li className='p-4'>Home</li>
+        <li className='p-4'>About</li>
+        <li className='p-4'>Contact</li>
+      </ul>
+      <div onClick={handleNav} className='block md:hidden'>
+        {!nav? <AiOutlineClose size={20}/>:<AiOutlineMenu size={20}/>}
+
+      </div>
+      <div className={!nav?'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full ease-in-out duration-500':'fixed left-[-100%]'}>
+        <ul className='pt-24 uppercase'>
+          <li className='p-4 border-b border-gray-600'>Home</li>
+          <li className='p-4 border-b border-gray-600'>About</li>
+          <li className='p-4 '>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
