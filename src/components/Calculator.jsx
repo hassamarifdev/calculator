d2e3477b22ffe9c94db08124dd3cbe5e5f8363e3
hassamarif <arifhassam18@gmail.com>
2023-03-26 03:46:39 +0500

    updates pushed

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Calculator() {
-  return (
+
+
+  const [number, newNumber] = useState('')
+
+  // Handle click
+  const handleClick=()=>{
+    setNumber(number.concat(e.target.value))
+  }
+
+
+  return ( 
     <React.Fragment>
       <sectioin>
         <div className="bg-[#000300] h-screen w-96 mx-auto  overflow-hidden mt-10">
           <div className="w-full h-20">
-          <input type="text" placeholder="0" className="w-full h-full border rounded-lg border-white bg-[#000300] text-4xl text-right pr-5"/>
+          <input 
+          type="text" 
+          placeholder="0" 
+          value= {number} 
+          className="w-full h-full border rounded-lg border-white bg-[#000300] text-4xl text-right pr-5"/>
           </div>
           <div className="grid grid-cols-4 gap-2 mt-10 mx-3">
-            <button className="text-gray w-20 h-20 bg-[#59dda8] rounded-lg font-bold text-4xl flex justify-center items-center shadow">AC</button>
-            <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">C</button>
-            <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">/</button>
-            <button className="text-white w-20 h-42 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow row-span-2">+</button>
+            <button onClick={handleClick} className="text-gray w-20 h-20 bg-[#59dda8] rounded-lg font-bold text-4xl flex justify-center items-center shadow">AC</button>
+            <button className="text-gray w-20 h-20 bg-[#59dda8] rounded-lg font-bold text-4xl flex justify-center items-center shadow">C</button>
+            <button className="text-gray w-20 h-20 bg-[#59dda8] rounded-lg font-bold text-4xl flex justify-center items-center shadow">/</button>
+            <button className="text-gray w-20 h-42 bg-[#59dda8] rounded-lg font-bold text-4xl flex justify-center items-center shadow row-span-2">+</button>
 
             <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">9</button>
             <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">8</button>
@@ -21,16 +35,16 @@ export default function Calculator() {
             <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">4</button>
             <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">5</button>
             <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">6</button>
-            <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">-</button>
+            <button className="text-gray w-20 h-20 bg-[#59dda8] rounded-lg font-bold text-4xl flex justify-center items-center shadow">-</button>
 
             <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">1</button>
             <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">2</button>
             <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">3</button>
-            <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">*</button>
+            <button className="text-gray w-20 h-20 bg-[#59dda8] rounded-lg font-bold text-4xl flex justify-center items-center shadow">*</button>
 
             <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">.</button>
             <button className="text-white w-20 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow">0</button>
-            <button className="text-white w-44 h-20 bg-indigo-500 rounded-lg font-bold text-4xl flex justify-center items-center shadow col-span-2">*</button>
+            <button className="text-gray w-[171px] h-20 bg-[#59dda8] rounded-lg font-bold text-4xl flex justify-center items-center shadow col-span-2">*</button>
           </div>
         </div>
       </sectioin>
